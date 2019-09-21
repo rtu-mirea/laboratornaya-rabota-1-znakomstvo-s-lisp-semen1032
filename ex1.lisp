39aff4ff5bfbc7866897bc70634665668d35424c
semen1032 <51064226+semen1032@users.noreply.github.com>
2019-09-21 11:18:27 +0300

    lisp exercise

---
+++ ex1.lisp
@@ -0,0 +1,8 @@
+(defun factorial(n)
+(if (= n 0)
+1
+(* n (factorial (- n 1)))))
+
+(loop for i from 1 to 16
+do (format t "~D," (factorial i))
+finally (format t ",,~%"))
---
+++ ex3.lisp
@@ -0,0 +1,1 @@
+(car(list (list 8 10) (list 9 1) (list 2 8) (list 3 7) (list 6 4)))
