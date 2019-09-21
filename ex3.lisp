48f5911f8b25e9fc2bc11775fffd4a869e8eab14
semen1032 <51064226+semen1032@users.noreply.github.com>
2019-09-21 11:25:39 +0300

    ex3 changed

--- ex3.lisp
+++ ex3.lisp
@@ -1,1 +1,5 @@
-(car(list (list 8 10) (list 9 1) (list 2 8) (list 3 7) (list 6 4)))
+(cdr (nth 3 (list (list 8 10) (list 9 1) (list 2 8) (list 3 7) (list 6 4))))
+
+(car (nth 2 (list (list 8 10) (list 9 1) (list 2 8) (list 3 7) (list 6 4))))
+
+(nth 4 (list (list 8 10) (list 9 1) (list 2 8) (list 3 7) (list 6 4)))
